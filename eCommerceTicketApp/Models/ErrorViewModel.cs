f71844e4f4cc7f1336060821cc8130706f1226b0
Jay-Envy <74972940+Jay-Envy@users.noreply.github.com>
2023-04-12 10:30:26 +0200

    Add project files.

---
+++ eCommerceTicketApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace eCommerceTicketApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
